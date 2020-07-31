72a466481f0b8563240a8c7c3f48f85b0e640961
Martha Sharpe <freedom@Nats-MacBook-Pro.local>
2020-07-31 15:45:02 -0400

    remove local state for onCallSwitch

--- components/OnCallSwitch.tsx
+++ components/OnCallSwitch.tsx
@@ -1,40 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Text, Switch } from 'react-native';
 import { useDispatch } from "react-redux";
 import { setDriverOnCallState } from "../actions/driverActions";
 import {  DsprDriver } from "../store/reduxStoreState";
 import Colors from '../constants/Colors';
 
-type SwitchProps = {
-    dsprDriver: DsprDriver;
-}
+type SwitchProps = { dsprDriver: DsprDriver; }
 
 const OnCallSwitch = ({dsprDriver}: SwitchProps) => {
-    
-    const [ isOnCall, setIsOnCall ] = useState(dsprDriver.onCall);
-  
+      
     const dispatch = useDispatch();
-  
-    const setOnCallState = () => {
-      let onCallString = isOnCall ? 'on' : null;
-      dispatch(setDriverOnCallState(dsprDriver.id, onCallString));
-    }
     
     const toggleSwitch = () => {
-      setIsOnCall(!isOnCall);
-      setOnCallState();
+        let onCallString = !dsprDriver.onCall ? 'on' : null;
+        dispatch(setDriverOnCallState(dsprDriver.id, onCallString));  
     }
 
     return (
         <>
-            <Switch
-                trackColor={{ false: Colors.red, true: Colors.green }}
-                thumbColor={dsprDriver.onCall ? "#ffffff" : "#ffffff"}
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={toggleSwitch}
-                value={dsprDriver.onCall}
-            />
-            <Text>{dsprDriver.onCall ? 'On Call' : 'Not on Call'}</Text>
+        <Switch
+            trackColor={{ false: Colors.red, true: Colors.green }}
+            thumbColor={dsprDriver.onCall ? "#ffffff" : "#ffffff"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={dsprDriver.onCall}
+        />
+        <Text>{dsprDriver.onCall ? 'On Call' : 'Not on Call'}</Text>
         </>
     )
 };
