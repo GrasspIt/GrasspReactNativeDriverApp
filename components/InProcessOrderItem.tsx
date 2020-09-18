d7b2bbfec264f4ee6142f442a80b3073c7b1a33d
Martha Sharpe <marthasharpe2020@gmail.com>
2020-09-18 18:09:48 -0400

    add linking to phone and text

--- components/InProcessOrderItem.tsx
+++ components/InProcessOrderItem.tsx
@@ -1,27 +1,45 @@
 import React from 'react';
 import { ListItem } from 'react-native-elements';
-import { Text } from 'react-native';
+import { Alert, Text } from 'react-native';
 import { formatPhone } from '../hooks/util';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import * as Linking from 'expo-linking';
 
 const InProcessOrderItem = ({ orderInfo, navigation }) => {
   const handleNavigate = () => {
     navigation.navigate('Details', { order: orderInfo });
   };
+
+  const handlePhone = () => {
+    Alert.alert(
+      'Contact Customer',
+      `How would you like to contact ${formatPhone(orderInfo.user.phoneNumber)}`,
+      [
+        { text: 'Call', onPress: () => Linking.openURL(`tel:${orderInfo.user.phoneNumber}`) },
+        { text: 'Text', onPress: () => Linking.openURL(`sms:${orderInfo.user.phoneNumber}`) },
+      ]
+    );
+  };
+
   return (
     orderInfo && (
-      <ListItem style={{ paddingLeft: 10 }} onPress={handleNavigate}>
+      <ListItem style={{ paddingLeft: 10 }}>
         <ListItem.Content>
           <ListItem.Title style={{ fontSize: 18 }}>
             {orderInfo.user.firstName} {orderInfo.user.lastName},{' '}
             <Text style={{ fontSize: 16 }}>${orderInfo.cashTotal}</Text>
           </ListItem.Title>
-          <ListItem.Subtitle style={{ fontSize: 16, paddingVertical: 6 }}>
-            {orderInfo.address.street} {orderInfo.address.zipCode}
-          </ListItem.Subtitle>
-          {orderInfo.orderStatus == 'in_process' ? (
-            <ListItem.Subtitle style={{ fontSize: 16 }}>
-              {formatPhone(orderInfo.user.phoneNumber)}
+          <TouchableOpacity>
+            <ListItem.Subtitle style={{ fontSize: 16, paddingVertical: 6, color: 'blue' }}>
+              {orderInfo.address.street} {orderInfo.address.zipCode}
             </ListItem.Subtitle>
+          </TouchableOpacity>
+          {orderInfo.orderStatus == 'in_process' ? (
+            <TouchableOpacity onPress={handlePhone}>
+              <ListItem.Subtitle style={{ fontSize: 16, color: 'blue' }}>
+                {formatPhone(orderInfo.user.phoneNumber)}
+              </ListItem.Subtitle>
+            </TouchableOpacity>
           ) : null}
         </ListItem.Content>
         <ListItem.Chevron />
