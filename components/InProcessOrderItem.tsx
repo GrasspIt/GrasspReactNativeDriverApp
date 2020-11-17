ee6d94e28a88b00a6fcf940306160b923ff8c214
Martha Sharpe <marthasharpe2020@gmail.com>
2020-11-17 10:25:12 -0500

    add chevron to in process order

--- components/InProcessOrderItem.tsx
+++ components/InProcessOrderItem.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Alert, Text, Platform } from 'react-native';
+import { Alert, Text, Platform, View } from 'react-native';
 import { formatPhone } from '../utils/util';
 import * as Linking from 'expo-linking';
-import { useTheme, Card, Button } from 'react-native-paper';
+import { useTheme, Card, Button, IconButton } from 'react-native-paper';
 
 const InProcessOrderItem = ({ orderInfo, navigation }) => {
   const { colors } = useTheme();
@@ -34,29 +34,40 @@ const InProcessOrderItem = ({ orderInfo, navigation }) => {
         style={{ marginHorizontal: 10 }}
         onPress={() => navigation.navigate('Details', { orderId: orderInfo.id })}
       >
-        <Card.Content>
-          <Text style={{ fontSize: 18, paddingLeft: 16, paddingBottom: 4 }}>
-            {orderInfo.user.firstName} {orderInfo.user.lastName},
-          </Text>
-          <Button
-            mode='text'
-            onPress={handleMap}
-            uppercase={false}
-            color='#0000EE'
-            labelStyle={{ fontSize: 16 }}
-            style={{ alignSelf: 'flex-start' }}
-          >
-            {orderInfo.address.street} {orderInfo.address.zipCode}
-          </Button>
-          <Button
-            mode='text'
-            onPress={handlePhone}
-            color='#0000EE'
-            labelStyle={{ fontSize: 16 }}
-            style={{ alignSelf: 'flex-start' }}
-          >
-            {formatPhone(orderInfo.user.phoneNumber)}
-          </Button>
+        <Card.Content
+          style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}
+        >
+          <View>
+            <View style={{ flexDirection: 'row', alignItems: 'baseline' }}>
+              <Text style={{ fontSize: 18, paddingLeft: 16, paddingBottom: 4 }}>
+                {orderInfo.user.firstName} {orderInfo.user.lastName},{' '}
+              </Text>
+              <Text style={{ fontSize: 14, paddingBottom: 4 }}>${orderInfo.cashTotal}</Text>
+            </View>
+            <Button
+              mode='text'
+              onPress={handleMap}
+              uppercase={false}
+              color='#0000EE'
+              labelStyle={{ fontSize: 16 }}
+              style={{ alignSelf: 'flex-start' }}
+            >
+              {orderInfo.address.street} {orderInfo.address.zipCode}
+            </Button>
+            <Button
+              mode='text'
+              onPress={handlePhone}
+              color='#0000EE'
+              labelStyle={{ fontSize: 16 }}
+              style={{ alignSelf: 'flex-start' }}
+            >
+              {formatPhone(orderInfo.user.phoneNumber)}
+            </Button>
+          </View>
+          <IconButton
+            icon='chevron-right'
+            onPress={() => navigation.navigate('Details', { orderId: orderInfo.id })}
+          />
         </Card.Content>
       </Card>
     )
