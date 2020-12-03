f54dad5750a0fd7fe0b7407deee25013d852f664
Martha Sharpe <marthasharpe2020@gmail.com>
2020-12-03 18:54:30 -0500

    add icon buttons to in process order

--- components/InProcessOrderItem.tsx
+++ components/InProcessOrderItem.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Alert, Text, Platform, View } from 'react-native';
 import { formatPhone } from '../utils/util';
 import * as Linking from 'expo-linking';
-import { Card, Button, IconButton } from 'react-native-paper';
+import { Card, Button, IconButton, useTheme, List } from 'react-native-paper';
 
 const InProcessOrderItem = ({ orderInfo, navigation }) => {
+  const { colors } = useTheme();
   const handleMap = () => {
     let daddr = encodeURIComponent(`${orderInfo.address.street} ${orderInfo.address.zipCode}`);
     if (Platform.OS === 'ios') {
@@ -35,32 +36,52 @@ const InProcessOrderItem = ({ orderInfo, navigation }) => {
         <Card.Content
           style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}
         >
-          <View>
-            <View style={{ flexDirection: 'row', alignItems: 'baseline' }}>
-              <Text style={{ fontSize: 18, paddingLeft: 16, paddingBottom: 4 }}>
-                {orderInfo.user.firstName} {orderInfo.user.lastName},{' '}
-              </Text>
-              <Text style={{ fontSize: 14, paddingBottom: 4 }}>${orderInfo.cashTotal}</Text>
-            </View>
-            <Button
-              mode='text'
-              onPress={handleMap}
-              uppercase={false}
-              color='#0000EE'
-              labelStyle={{ fontSize: 16 }}
-              style={{ alignSelf: 'flex-start' }}
-            >
-              {orderInfo.address.street} {orderInfo.address.zipCode}
-            </Button>
-            <Button
-              mode='text'
-              onPress={handlePhone}
-              color='#0000EE'
-              labelStyle={{ fontSize: 16 }}
-              style={{ alignSelf: 'flex-start' }}
-            >
-              {formatPhone(orderInfo.user.phoneNumber)}
-            </Button>
+          <View style={{ flex: 1 }}>
+            <List.Item
+              title={
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'baseline',
+                    paddingVertical: 0,
+                    paddingLeft: 34,
+                  }}
+                >
+                  <Text style={{ fontSize: 18 }}>
+                    {orderInfo.user.firstName} {orderInfo.user.lastName},{' '}
+                  </Text>
+                  <Text style={{ fontSize: 14 }}>${orderInfo.cashTotal}</Text>
+                </View>
+              }
+              style={{ paddingVertical: 0 }}
+            />
+            {orderInfo.address.aptNumber ? (
+              <List.Item
+                title={`${orderInfo.address.street}, ${orderInfo.address.zipCode}, Unit ${orderInfo.address.aptNumber}`}
+                titleNumberOfLines={2}
+                style={{ padding: 0 }}
+                left={() => (
+                  <IconButton icon='map' color={colors.primary} size={20} onPress={handleMap} />
+                )}
+              />
+            ) : (
+              <List.Item
+                title={`${orderInfo.address.street}, ${orderInfo.address.zipCode}`}
+                titleNumberOfLines={2}
+                style={{ padding: 0 }}
+                left={() => (
+                  <IconButton icon='map' color={colors.primary} size={20} onPress={handleMap} />
+                )}
+              />
+            )}
+            <List.Item
+              title={`${formatPhone(orderInfo.user.phoneNumber)}`}
+              titleNumberOfLines={2}
+              style={{ padding: 0 }}
+              left={() => (
+                <IconButton icon='phone' color={colors.primary} size={20} onPress={handlePhone} />
+              )}
+            />
           </View>
           <IconButton
             icon='chevron-right'
