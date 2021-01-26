bc485a1d1f8c1b02fdbe2ededfada9f4a829280b
Martha Sharpe <marthasharpe2020@gmail.com>
2021-01-26 14:33:05 -0600

    create ManageNotesDisplay as dummy component separate from ManageNotesScreen

---
+++ components/ManageNotesDisplay.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { ScrollView, View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { List, useTheme, FAB, Card, IconButton } from 'react-native-paper';
+import NewUserNoteForm from '../components/NewUserNoteForm';
+import Moment from 'moment';
+
+type Props = {
+  userNotes;
+  hideUserNote;
+  unhideUserNote;
+  showNotes;
+  setShowNotes;
+  handleNewNoteSubmit;
+};
+
+const ManageNotesDisplay = ({
+  hideUserNote,
+  unhideUserNote,
+  userNotes,
+  showNotes,
+  setShowNotes,
+  handleNewNoteSubmit,
+}: Props) => {
+  const { colors } = useTheme();
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: colors.background }}>
+        {userNotes && userNotes.length > 0 ? (
+          <ScrollView style={{ flex: 1, backgroundColor: colors.background }}>
+            {userNotes.map((userNote) => (
+              <>
+                <Card key={userNote.id} style={{ marginHorizontal: 10, marginTop: 10 }}>
+                  <Card.Content>
+                    <List.Item
+                      key={userNote.id}
+                      title={`${userNote.note}`}
+                      description={`${Moment(userNote.createdTimestamp).format(
+                        'MMMM Do YYYY, h:mm a'
+                      )}`}
+                      descriptionStyle={{ alignSelf: 'flex-end' }}
+                      titleNumberOfLines={3}
+                    />
+                  </Card.Content>
+                  <Card.Actions>
+                    {!userNote.isVisible ? (
+                      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <IconButton
+                          icon='checkbox-blank-outline'
+                          color={colors.error}
+                          size={22}
+                          onPress={() => unhideUserNote(userNote.id)}
+                        />
+                        <Text>not visible</Text>
+                      </View>
+                    ) : (
+                      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <IconButton
+                          icon='check-box-outline'
+                          color={colors.primary}
+                          size={22}
+                          onPress={() => hideUserNote(userNote.id)}
+                        />
+                        <Text>visible</Text>
+                      </View>
+                    )}
+                  </Card.Actions>
+                </Card>
+              </>
+            ))}
+          </ScrollView>
+        ) : (
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>No User Notes</Text>
+          </View>
+        )}
+        <FAB
+          style={styles.fab}
+          color={colors.surface}
+          label='Create Note'
+          icon='plus'
+          onPress={() => setShowNotes(true)}
+        />
+        <NewUserNoteForm
+          showNotes={showNotes}
+          closeDialog={() => setShowNotes(false)}
+          onSubmit={handleNewNoteSubmit}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+});
+
+export default ManageNotesDisplay;
--- screens/ManageNotesScreen.tsx
+++ screens/ManageNotesScreen.tsx
@@ -1,12 +1,6 @@
 import React, { useState } from 'react';
-import { ScrollView, View, Text, StyleSheet, SafeAreaView } from 'react-native';
-import { List, useTheme, FAB, Card, IconButton } from 'react-native-paper';
-import NewUserNoteForm from '../components/NewUserNoteForm';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { OrderListStackParamsList } from '../navigation/OrderListNavigator';
 import { connect } from 'react-redux';
 import { getUserNotesFromProps } from '../selectors/userSelectors';
-import Moment from 'moment';
 
 import {
   getSpecificUser,
@@ -14,12 +8,9 @@ import {
   hideUserNote,
   unhideUserNote,
 } from '../actions/userActions';
-
-type ManageNotesScreenNavigationProp = StackNavigationProp<OrderListStackParamsList, 'Notes'>;
+import ManageNotesDisplay from '../components/ManageNotesDisplay';
 
 type Props = {
-  navigation: ManageNotesScreenNavigationProp;
-  route;
   userNotes;
   userId: number;
   dsprDriverId?: number;
@@ -31,8 +22,6 @@ type Props = {
 };
 
 const ManageNotes = ({
-  navigation,
-  route,
   createUserNote,
   hideUserNote,
   unhideUserNote,
@@ -41,7 +30,6 @@ const ManageNotes = ({
   dsprDriverId,
   userId,
 }: Props) => {
-  const { colors } = useTheme();
   const [showNotes, setShowNotes] = useState(false);
 
   const handleNewNoteSubmit = (values) => {
@@ -51,81 +39,16 @@ const ManageNotes = ({
   };
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: colors.background }}>
-        {userNotes && userNotes.length > 0 ? (
-          <ScrollView style={{ flex: 1, backgroundColor: colors.background }}>
-            {userNotes.map((userNote) => (
-              <>
-                <Card key={userNote.id} style={{ marginHorizontal: 10, marginTop: 10 }}>
-                  <Card.Content>
-                    <List.Item
-                      key={userNote.id}
-                      title={`${userNote.note}`}
-                      description={`${Moment(userNote.createdTimestamp).format(
-                        'MMMM Do YYYY, h:mm a'
-                      )}`}
-                      descriptionStyle={{ alignSelf: 'flex-end' }}
-                      titleNumberOfLines={3}
-                    />
-                  </Card.Content>
-                  <Card.Actions>
-                    {!userNote.isVisible ? (
-                      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <IconButton
-                          icon='checkbox-blank-outline'
-                          color={colors.error}
-                          size={22}
-                          onPress={() => unhideUserNote(userNote.id)}
-                        />
-                        <Text>not visible</Text>
-                      </View>
-                    ) : (
-                      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <IconButton
-                          icon='check-box-outline'
-                          color={colors.primary}
-                          size={22}
-                          onPress={() => hideUserNote(userNote.id)}
-                        />
-                        <Text>visible</Text>
-                      </View>
-                    )}
-                  </Card.Actions>
-                </Card>
-              </>
-            ))}
-          </ScrollView>
-        ) : (
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>No User Notes</Text>
-          </View>
-        )}
-        <FAB
-          style={styles.fab}
-          color={colors.surface}
-          label='Create Note'
-          icon='plus'
-          onPress={() => setShowNotes(true)}
-        />
-        <NewUserNoteForm
-          showNotes={showNotes}
-          closeDialog={() => setShowNotes(false)}
-          onSubmit={handleNewNoteSubmit}
-        />
-      </View>
-    </SafeAreaView>
+    <ManageNotesDisplay
+      userNotes={userNotes}
+      unhideUserNote={unhideUserNote}
+      hideUserNote={hideUserNote}
+      showNotes={showNotes}
+      setShowNotes={setShowNotes}
+      handleNewNoteSubmit={handleNewNoteSubmit}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  fab: {
-    position: 'absolute',
-    margin: 16,
-    right: 0,
-    bottom: 0,
-  },
-});
 
 const mapStateToProps = (state, route) => {
   const { userId, dsprDriverId } = route.route.params;
