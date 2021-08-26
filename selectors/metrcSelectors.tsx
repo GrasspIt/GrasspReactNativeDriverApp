75f15d438443b290b211c1c9532eb95fe05b4237
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2021-08-26 15:25:51 -0700

    Update return for getMetrcScansForOrderFromProps
    - selector now returns an empty object, rather than undefined, if there are not any scans for the order.
    - this change allows the selectors for metrcScansForOrder and metrcScansForOrderDetail to have a similar output (if there are no scans for an orderDetail, and empty [] is returned

--- selectors/metrcSelectors.tsx
+++ selectors/metrcSelectors.tsx
@@ -2,9 +2,8 @@ import { MetrcTag, State } from '../store/reduxStoreState';
 import { createSelector } from 'reselect';
 
 export const getMetrcScansForOrderDetailFromProps = (state: State, {orderId, orderDetailId}): MetrcTag[] => {
-    return state.api.entities.metrcTagsForOrder[orderId] && state.api.entities.metrcTagsForOrder[orderId][orderDetailId]
-        ? state.api.entities.metrcTagsForOrder[orderId][orderDetailId]
-        : [];
+    const metrcScansForOrderDetail = state.api.entities.metrcTagsForOrder[orderId] && state.api.entities.metrcTagsForOrder[orderId][orderDetailId]
+    return metrcScansForOrderDetail ? metrcScansForOrderDetail : [];
 }
 
 export const getMetrcScanCountForOrderDetailFromProps = (state: State, {orderId, orderDetailId}): number => {
@@ -13,14 +12,15 @@ export const getMetrcScanCountForOrderDetailFromProps = (state: State, {orderId,
 }
 
 export const getMetrcScansForOrderFromProps = (state: State, {orderId}): { [orderId: number]: MetrcTag[]} => {
-    return state.api.entities.metrcTagsForOrder[orderId]
+    const metrcScansForOrder = state.api.entities.metrcTagsForOrder[orderId]
+    return metrcScansForOrder ? metrcScansForOrder : {};
 }
 
 export const getMetrcScanCountForOrderFromProps = (state: State, {orderId}): number => {
     const metrcScansForOrder = getMetrcScansForOrderFromProps(state, {orderId});
     let scanCount = 0;
 
-    if (Object.keys(metrcScansForOrder).length > 0) {
+    if (metrcScansForOrder && Object.keys(metrcScansForOrder).length > 0) {
         for (let orderDetailId in metrcScansForOrder) {
             scanCount += metrcScansForOrder[orderDetailId].length;
         }
