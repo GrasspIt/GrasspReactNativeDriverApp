7e97d4a98a69271dbef1de3c0d058d919f330bf7
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2021-09-02 22:52:28 -0700

    Write action creator to submit Metrc Tag
    - Action dispatched on scan of a tag

--- actions/metrcActions.tsx
+++ actions/metrcActions.tsx
@@ -1,7 +1,38 @@
+import { CALL_API, Schemas } from "../middleware/api";
+import { Alert } from "react-native";
 
-export const SCAN_METRC_TAG = 'SCAN_METRC_TAG';
-export const SCAN_METRC_TAG_SUCCESS = 'SCAN_METRC_TAG_SUCCESS';
-export const SCAN_METRC_TAG_FAILURE = 'SCAN_METRC_TAG_FAILURE';
+export const METRC_TAG_SUBMIT_PENDING = 'METRC_TAG_SUBMIT_PENDING';
+export const METRC_TAG_SUBMIT = 'METRC_TAG_SUBMIT';
+export const METRC_TAG_SUBMIT_SUCCESS = 'METRC_TAG_SUBMIT_SUCCESS';
+export const METRC_TAG_SUBMIT_FAILURE = 'METRC_TAG_SUBMIT_FAILURE';
+
+const metrcTagSubmitter = (metrcTag: string, orderId: number, productId: number, orderDetailId: number) => {
+    return {
+        [CALL_API]: {
+            httpAction: 'POST',
+            types: [METRC_TAG_SUBMIT, METRC_TAG_SUBMIT_SUCCESS, METRC_TAG_SUBMIT_FAILURE],
+            endPoint: 'metrc/scan_barcode',
+            body: {
+                metrcTag,
+                orderId,
+                productId,
+                orderDetailId
+            },
+            schema: Schemas.ORDER,
+        },
+    }
+}
+
+export const submitMetrcTag = (metrcTag: string, orderId: number, productId: number, orderDetailId: number) => (dispatch) => {
+    dispatch({type: METRC_TAG_SUBMIT_PENDING});
+    dispatch(metrcTagSubmitter(metrcTag, orderId, productId, orderDetailId))
+        .then((response) => {
+            if (response.type === METRC_TAG_SUBMIT_FAILURE) {
+                Alert.alert('Failed to Submit Metrc Tag', response.error);
+            }
+        })
+        .catch((error) => Alert.alert('Network error:', error));
+}
 
 export const RESET_METRC_ORDER_SCANS = 'RESET_METRC_ORDER_SCANS';
 export const RESET_METRC_ORDER_SCANS_SUCCESS = 'RESET_METRC_ORDER_SCANS_SUCCESS';
