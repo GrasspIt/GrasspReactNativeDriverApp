342dfa52c5ec6892a567e7a4e70975efc6582cf3
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2021-09-08 16:57:53 -0700

    Write selector to determine if dspr has metrc license

--- selectors/dsprSelectors.tsx
+++ selectors/dsprSelectors.tsx
@@ -3,3 +3,8 @@ import { State } from '../store/reduxStoreState';
 export const getDSPRs = (state: State) => state.api.entities.DSPRs;
 
 export const getDSPRFromProps = (state: State, props) => state.api.entities.DSPRs[props.dsprId];
+
+export const isMetrcLicenseHeldByDSPRFromProps = (state: State, props: {dsprId}) => {
+    const dspr = getDSPRFromProps(state, props);
+    return !!(dspr && dspr.metrcLicense);
+}
