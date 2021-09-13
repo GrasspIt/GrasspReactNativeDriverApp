121e396d2d9f71a3896074da0d2d4924d0a5a52b
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2021-09-13 16:32:11 -0700

    Refactor part of metrcSelectors.tsx
    - Refactored getOrderScansForOrderFromProps selector. Now returns{ [orderDetailId: number]: OrderScan[]}
    - Added getOrderScans and getOrderScanIdsForOrderFromProps selectors
    - Renamed selectors to remove metrc reference (selectors may be used for non-metrc barcodes)

--- selectors/metrcSelectors.tsx
+++ selectors/metrcSelectors.tsx
@@ -1,24 +1,62 @@
-import { MetrcTag, State } from '../store/reduxStoreState';
+import { OrderScan, State } from '../store/reduxStoreState';
 import { createSelector } from 'reselect';
+import { getProductsForDSP } from "./dspProductSelector";
+import { getOrderFromProps } from "./orderSelectors";
 
-export const getMetrcScansForOrderDetailFromProps = (state: State, {orderId, orderDetailId}): MetrcTag[] => {
-    const metrcScansForOrderDetail = state.api.entities.metrcTagsForOrder[orderId] && state.api.entities.metrcTagsForOrder[orderId][orderDetailId]
+//export const getProductsForDSPForAutoSelect = createSelector([getProductsForDSP], (products) => {
+//    return products
+//        ? products.map((product) => {
+//            return { value: product.id, text: product.name };
+//        })
+//        : [];
+//});
+
+export const getOrderScans = (state: State): {[orderScanId: number]: OrderScan} => {
+    return state.api.entities.orderScans;
+}
+
+/**Returns an array of OrderScan objects for a particular order detail*/
+export const getOrderScansForOrderDetailFromProps = (state: State, {orderId, orderDetailId}): OrderScan[] => {
+    const metrcScansForOrderDetail = state.api.entities.orderScans[orderId] && state.api.entities.metrcTagsForOrder[orderId][orderDetailId]
     return metrcScansForOrderDetail ? metrcScansForOrderDetail : [];
 }
 
-export const getMetrcScanCountForOrderDetailFromProps = (state: State, {orderId, orderDetailId}): number => {
-    const metrcScansForOrderDetail = getMetrcScansForOrderDetailFromProps(state, {orderId, orderDetailId});
+/**Returns count of scans for a particular order detail*/
+export const getOrderScanCountForOrderDetailFromProps = (state: State, {orderId, orderDetailId}): number => {
+    const metrcScansForOrderDetail = getOrderScansForOrderDetailFromProps(state, {orderId, orderDetailId});
     return metrcScansForOrderDetail.length
 }
 
-export const getMetrcScansForOrderFromProps = (state: State, {orderId}): { [orderId: number]: MetrcTag[]} => {
-    const metrcScansForOrder = state.api.entities.metrcTagsForOrder[orderId]
-    return metrcScansForOrder ? metrcScansForOrder : {};
+const getOrderScanIdsForOrderFromProps = (state: State, {orderId}): number[] => {
+    const order = getOrderFromProps(state, {orderId});
+    return order && order.metrcOrderDetailAssociationScans ? order.metrcOrderDetailAssociationScans : [];
 }
 
+/**Returns an object of orderDetailIds and OrderScan objects {[orderDetailId]: [OrderScan]}*/
+export const getOrderScansForOrderFromProps = (state: State, {orderId}): { [orderDetailId: number]: OrderScan[] } => {
+    const orderScanIds = getOrderScanIdsForOrderFromProps(state, {orderId});
+    const allOrderScans = getOrderScans(state);
+
+    if (orderScanIds.length > 0 && orderScanIds.length > 0) {
+        const scansForOrder = {};
+        orderScanIds.forEach(scanId => {
+            const orderScan = allOrderScans[scanId];
+            const orderDetailId = orderScan.orderDetail;
+            scansForOrder[orderDetailId]
+                ? scansForOrder[orderDetailId] = scansForOrder[orderDetailId].push(orderScan)
+                : [orderScan];
+        })
+
+        return scansForOrder;
+    }
+
+    return {};
+}
+
+/**Returns count of scans for an order*/
 export const getMetrcScanCountForOrderFromProps = (state: State, {orderId}): number => {
     console.log('getMetrcScanCountForOrderFromProps Running!!!')
-    const metrcScansForOrder = getMetrcScansForOrderFromProps(state, {orderId});
+    const metrcScansForOrder = getOrderScansForOrderFromProps(state, {orderId});
 
     return Object.values(metrcScansForOrder).reduce(((acc, currVal) => acc + currVal.length), 0)
     //let scanCount = 0;
