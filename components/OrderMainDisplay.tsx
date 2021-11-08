49d965239ef1781df0bbba3a95099568a48ebb09
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2021-11-08 01:22:11 -0800

    Implement basic version of OrderMainDisplay
    - Will function similarly to RoutingMainDisplay - will render different components based upon whether user selects "map" or "list"

---
+++ components/OrderMainDisplay.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { ActivityIndicator, useTheme } from "react-native-paper";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import RouteAndOrderViewButtons from "./RouteAndOrderViewButtons";
+import OrderList from "./OrderList";
+
+type OrderMainDisplayProps = {
+    navigation;
+    dsprDriver;
+    isLoading;
+    getDriverData;
+    removeOrderAndRefreshRoute;
+    deactivateDriverRoute;
+    markOrderInProcess;
+    cancelOrder;
+    orderView: 'list' | 'map';
+};
+
+const OrderMainDisplay = ({
+                              navigation,
+                              dsprDriver,
+                              isLoading,
+                              getDriverData,
+                              removeOrderAndRefreshRoute,
+                              deactivateDriverRoute,
+                              markOrderInProcess,
+                              cancelOrder,
+                              orderView,
+                          }: OrderMainDisplayProps) => {
+    const {colors} = useTheme();
+
+    return (
+        <SafeAreaView style={{flex: 1}}>
+            {isLoading ? (
+                <View style={styles.container}>
+                    <ActivityIndicator size='large' color={colors.primary}/>
+                </View>
+            ) : dsprDriver && dsprDriver.currentRoute && dsprDriver.currentRoute.active ? (
+                <View style={{flex: 1}}>
+                    <RouteAndOrderViewButtons view={orderView} setView={setOrderView}/>
+                    {orderView === 'list' ? (
+                        <OrderList
+                            navigation={navigation}
+                            dsprDriver={dsprDriver}
+                            isLoading={isLoading}
+                            getDriverData={getDriverData}
+                            removeOrderAndRefreshRoute={removeOrderAndRefreshRoute}
+                            deactivateDriverRoute={deactivateDriverRoute}
+                            markOrderInProcess={markOrderInProcess}
+                            cancelOrder={cancelOrder}
+                        />
+                    ) : (
+                        <>
+                        {/*OrderMapView*/}
+                        null
+                        </>
+                    )
+                    }
+                </View>
+            ) : (
+                <View style={styles.container}>
+                    <Text style={{ padding: 10 }}>No current orders.</Text>
+                </View>
+            )}
+            </SafeAreaView>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
