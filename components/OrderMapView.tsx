7cc93778a98a2208c1fe49d1155f3cdd8b8b7618
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2021-11-30 13:29:29 -0800

    Fit all pins on screen in OrderMapView
    - The map automatically resizes to fit all order and driver pins

--- components/OrderMapView.tsx
+++ components/OrderMapView.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useLayoutEffect, useRef, useMemo, useCallback } from 'react';
 import { SafeAreaView, Text, StyleSheet, Dimensions } from 'react-native';
 import MapView, { Callout, Marker, Polyline } from 'react-native-maps';
 import { OrderWithAddressAndUser, RouteLeg, State } from '../store/reduxStoreState';
@@ -27,6 +27,11 @@ const OrderMapView = ({
                           dsprDriver,
                       }: OrderMapViewProps) => {
 
+    const mapRef = useRef<MapView>(null);
+
+    //const [orderMarkers, setOrderMarkers] = useState<any[]>([]);
+    const [mapIdentifiers, setMapIdentifiers] = useState<string[]>([]);
+
     const driverName = dsprDriver && dsprDriver.user && dsprDriver.user.firstName + ' ' + dsprDriver.user.lastName;
 
     // const orderAddresses = useSelector<State, QueuedAndInProcessOrdersWithAddressesForDriver>(
@@ -46,8 +51,71 @@ const OrderMapView = ({
     console.log('dsprDriver in OrderMapView:', dsprDriver);
 
 
-    const orderMarkers =
-        orderAddresses &&
+    const orderMarkers = useMemo(() => {
+        const identifiers: string[] = [];
+        if( orderAddresses &&
+        orderAddresses.length > 0 &&
+        orderAddresses) {
+            const markers = orderAddresses.map(
+                (
+                    order: OrderWithAddressAndUser
+                ) => {
+                    if (!order || !order.address) return null;
+                    const orderId = order.id.toString();
+                    identifiers.push(orderId);
+                    return (
+                        <Marker
+                            coordinate={{
+                                latitude: order.address.latitude,
+                                longitude: order.address.longitude,
+                            }}
+                            {...order}
+                            pinColor='red'
+                            key={order.address.id}
+                            identifier={orderId}
+                        >
+                            <Callout onPress={() => navigation.navigate('Details', {orderId: order.id})}>
+                                 <Text>{order.user.firstName + ' ' + order.user.lastName}</Text>
+                                <Text style={{color: '#2089dc'}}>Order Details</Text>
+                            </Callout>
+                        </Marker>
+                    );
+                }
+            )
+            .filter((marker) => marker != null);
+
+            identifiers.push(dsprDriver.id.toString());
+            setMapIdentifiers(identifiers);
+            return markers;
+        }
+    }, [orderAddresses]);
+
+    const onMapReadyHandler = useCallback(() => {
+        if (mapRef.current) {
+            mapRef.current.fitToSuppliedMarkers(mapIdentifiers, {
+                edgePadding:
+                    {
+                        top: 50,
+                        right: 50,
+                        bottom: 50,
+                        left: 50
+                    }
+
+            })
+        }
+    }, [mapRef, mapIdentifiers]);
+
+    // Call fitToSuppliedMarkers() method on the MapView after markers get updated
+    //useLayoutEffect(() => {
+    //    if (mapRef.current) {
+    //        // list of _id's must same that has been provided to the identifier props of the Marker
+    //        mapRef.current.fitToSuppliedMarkers(orderMarkers.map(({ _id }) => _id));
+    //    }
+    //}, [markers]);
+
+
+    /*
+    * orderAddresses &&
         orderAddresses.length > 0 &&
         orderAddresses
             .map(
@@ -74,10 +142,12 @@ const OrderMapView = ({
                 }
             )
             .filter((marker) => marker != null);
+    * */
 
     return (
         <SafeAreaView style={styles.container}>
             <MapView
+                ref={mapRef}
                 style={styles.map}
                 //initialRegion={
                 //    routeCenter && onOverview
@@ -94,10 +164,12 @@ const OrderMapView = ({
                 //        longitudeDelta: 0.1,
                 //    }
                 //}
+                onMapReady={onMapReadyHandler}
             >
                 {dsprDriver && dsprDriver.currentLocation && (
                     <Marker
                         pinColor='green'
+                        identifier={dsprDriver.id.toString()}
                         coordinate={{
                             latitude: dsprDriver.currentLocation.latitude,
                             longitude: dsprDriver.currentLocation.longitude,
