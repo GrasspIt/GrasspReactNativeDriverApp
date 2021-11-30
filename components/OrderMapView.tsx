1ce3f3122ec1372b4c53c3bad99df0ce82e49971
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2021-11-30 12:11:01 -0800

    Render order and driver pins in OrderMapView
    - initial region is still not set
    - mimics map in RouteMapView, but does not include polylines
    - tapping on an order pin will allow users to go to the order detail page for that order
    - order pins are green, driver pin is red

--- components/OrderMapView.tsx
+++ components/OrderMapView.tsx
@@ -3,8 +3,14 @@ import { SafeAreaView, Text, StyleSheet, Dimensions } from 'react-native';
 import MapView, { Callout, Marker, Polyline } from 'react-native-maps';
 import { OrderWithAddressAndUser, RouteLeg, State } from '../store/reduxStoreState';
 import { DSPRDRiverWithUserAndOrdersAndServiceAreasAndCurrentRoute } from "../selectors/dsprDriverSelector";
-import { getQueuedAndInProcessOrdersWithAddressesForDriverFromProps, getQueuedAndInProcessOrdersWithAddressesForDriver } from '../selectors/orderSelectors';
-import { useSelector } from 'react-redux';
+import {
+    getQueuedAndInProcessOrdersWithAddressesForDriverFromProps,
+    QueuedAndInProcessOrdersWithAddressesForDriver,
+    getQueuedAndInProcessOrdersWithAddressesForDriverAsArrayFromProps,
+    QueuedAndInProcessOrdersWithAddressesForDriverAsArray,
+    OrderWithAddress, getQueuedAndInProcessOrdersWithAddressesAndUsersForDriverAsArrayFromProps,
+} from '../selectors/orderSelectors';
+import { useSelector, shallowEqual } from 'react-redux';
 
 
 interface OrderMapViewProps {
@@ -17,17 +23,129 @@ interface OrderMapViewProps {
 }
 
 const OrderMapView = ({
-    navigation,
-    dsprDriver,
-}: OrderMapViewProps) => {
+                          navigation,
+                          dsprDriver,
+                      }: OrderMapViewProps) => {
 
-    const orderAddresses = useSelector<State, getQueuedAndInProcessOrdersWithAddressesForDriver>(state => getQueuedAndInProcessOrdersWithAddressesForDriverFromProps(state, {dsprDriverId: dsprDriver.id}))
+    const driverName = dsprDriver && dsprDriver.user && dsprDriver.user.firstName + ' ' + dsprDriver.user.lastName;
 
-    console.log('orderAddresses in OrderMapView:', orderAddresses);
+    // const orderAddresses = useSelector<State, QueuedAndInProcessOrdersWithAddressesForDriver>(
+    //     state => getQueuedAndInProcessOrdersWithAddressesForDriverFromProps(state, {dsprDriverId: dsprDriver.id}), shallowEqual)
 
-    return(
-        <></>
+    // console.log('orderAddresses in OrderMapView:', orderAddresses);
+
+    const orderAddresses1 = useSelector<State, any>(
+        state => getQueuedAndInProcessOrdersWithAddressesForDriverAsArrayFromProps(state, {dsprDriverId: dsprDriver.id}), shallowEqual);
+
+    const orderAddresses = useSelector<State, OrderWithAddressAndUser[]>(
+        state => getQueuedAndInProcessOrdersWithAddressesAndUsersForDriverAsArrayFromProps(
+            state, {dsprDriverId: dsprDriver.id}),
+        shallowEqual);
+    console.log('orderAddresses1 as array:', orderAddresses1);
+
+    console.log('dsprDriver in OrderMapView:', dsprDriver);
+
+
+    const orderMarkers =
+        orderAddresses &&
+        orderAddresses.length > 0 &&
+        orderAddresses
+            .map(
+                (
+                    order: OrderWithAddressAndUser
+                ) => {
+                    if (!order || !order.address) return null;
+                    return (
+                        <Marker
+                            coordinate={{
+                                latitude: order.address.latitude,
+                                longitude: order.address.longitude,
+                            }}
+                            {...order}
+                            pinColor='red'
+                            key={order.address.id}
+                        >
+                            <Callout onPress={() => navigation.navigate('Details', {orderId: order.id})}>
+                                 <Text>{order.user.firstName + ' ' + order.user.lastName}</Text>
+                                <Text style={{color: '#2089dc'}}>Order Details</Text>
+                            </Callout>
+                        </Marker>
+                    );
+                }
+            )
+            .filter((marker) => marker != null);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <MapView
+                style={styles.map}
+                //initialRegion={
+                //    routeCenter && onOverview
+                //        ? {
+                //            latitude: routeCenter.lat,
+                //            longitude: routeCenter.lng,
+                //            latitudeDelta: 0.1,
+                //            longitudeDelta: 0.1,
+                //        }
+                //        : polylineCenter && {
+                //        latitude: polylineCenter.lat,
+                //        longitude: polylineCenter.lng,
+                //        latitudeDelta: 0.1,
+                //        longitudeDelta: 0.1,
+                //    }
+                //}
+            >
+                {dsprDriver && dsprDriver.currentLocation && (
+                    <Marker
+                        pinColor='green'
+                        coordinate={{
+                            latitude: dsprDriver.currentLocation.latitude,
+                            longitude: dsprDriver.currentLocation.longitude,
+                        }}
+                    >
+                        <Callout>
+                            <Text>{driverName}</Text>
+                            <Text>
+                                Outstanding Orders:{' '}
+                                {orderAddresses.length}
+                            </Text>
+                        </Callout>
+                    </Marker>
+                )}
+                {/*{!onOverview ? (*/}
+                {/*    <Polyline*/}
+                {/*        coordinates={orderPolylineCoordinates}*/}
+                {/*        geodesic={true}*/}
+                {/*        strokeColor='#03adfc'*/}
+                {/*        strokeWidth={5}*/}
+                {/*        lineDashPattern={[0]}*/}
+                {/*    />*/}
+                {/*) : (*/}
+                {/*    <Polyline*/}
+                {/*        coordinates={overviewPolylineCoordinates}*/}
+                {/*        geodesic={true}*/}
+                {/*        strokeColor='#03adfc'*/}
+                {/*        strokeWidth={5}*/}
+                {/*        lineDashPattern={[0]}*/}
+                {/*    />*/}
+                {/*)}*/}
+                {orderMarkers && orderMarkers.length > 0 && orderMarkers}
+            </MapView>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+});
 
 export default OrderMapView;
