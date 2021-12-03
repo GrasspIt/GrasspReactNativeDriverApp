a6cf60e3f4615a93190491056c62881160704720
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2021-12-03 14:24:37 -0800

    Implement action creator to fetch active metrc tags for dspr
    - will be used for the autocomplete in the manual entry of metrc tags for an order

--- actions/dsprActions.tsx
+++ actions/dsprActions.tsx
@@ -5,45 +5,45 @@ export const GET_DSPR_SUCCESS = 'GET_DSPR_SUCCESS';
 export const GET_DSPR_FAILURE = 'GET_DSPR_FAILURE';
 
 const dspr = (dsprId) => {
-  return {
-    [CALL_API]: {
-      httpAction: 'GET',
-      types: [GET_DSPR, GET_DSPR_SUCCESS, GET_DSPR_FAILURE],
-      endPoint: `dspr/${dsprId}`,
-      schema: Schemas.DSPR,
-    },
-  };
+    return {
+        [CALL_API]: {
+            httpAction: 'GET',
+            types: [GET_DSPR, GET_DSPR_SUCCESS, GET_DSPR_FAILURE],
+            endPoint: `dspr/${dsprId}`,
+            schema: Schemas.DSPR,
+        },
+    };
 };
 
 export const getDSPR = (dsprId) => (dispatch) => {
-  return dispatch(dspr(dsprId));
+    return dispatch(dspr(dsprId));
 };
 
 export const CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA =
-  'CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA';
+    'CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA';
 export const CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA_SUCCESS =
-  'CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA_SUCCESS';
+    'CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA_SUCCESS';
 export const CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA_FAILURE =
-  'CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA_FAILURE';
+    'CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA_FAILURE';
 
 const createOrUpdateADsprServiceArea = (dsprDriverServiceArea) => {
-  return {
-    [CALL_API]: {
-      httpAction: 'POST',
-      types: [
-        CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA,
-        CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA_SUCCESS,
-        CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA_FAILURE,
-      ],
-      endPoint: `dspr/driver-service-area`,
-      schema: Schemas.DSPR_DRIVER_SERVICE_AREA,
-      body: dsprDriverServiceArea,
-    },
-  };
+    return {
+        [CALL_API]: {
+            httpAction: 'POST',
+            types: [
+                CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA,
+                CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA_SUCCESS,
+                CREATE_OR_UPDATE_DSPR_DRIVER_SERVICE_AREA_FAILURE,
+            ],
+            endPoint: `dspr/driver-service-area`,
+            schema: Schemas.DSPR_DRIVER_SERVICE_AREA,
+            body: dsprDriverServiceArea,
+        },
+    };
 };
 
 export const createOrUpdateDsprServiceArea = (dsprDriverServiceArea) => (dispatch) => {
-  return dispatch(createOrUpdateADsprServiceArea(dsprDriverServiceArea));
+    return dispatch(createOrUpdateADsprServiceArea(dsprDriverServiceArea));
 };
 
 export const GET_DSPR_DRIVER_SERVICE_AREAS = 'GET_DSPR_DRIVER_SERVICE_AREAS';
@@ -51,24 +51,48 @@ export const GET_DSPR_DRIVER_SERVICE_AREAS_SUCCESS = 'GET_DSPR_DRIVER_SERVICE_AR
 export const GET_DSPR_DRIVER_SERVICE_AREAS_FAILURE = 'GET_DSPR_DRIVER_SERVICE_AREAS_FAILURE';
 
 const getDsprDriverServiceAreas = (dsprId: string) => {
-  const queryParamsMap = {
-    dspr_id: dsprId,
-  };
-  return {
-    [CALL_API]: {
-      httpAction: 'GET',
-      types: [
-        GET_DSPR_DRIVER_SERVICE_AREAS,
-        GET_DSPR_DRIVER_SERVICE_AREAS_SUCCESS,
-        GET_DSPR_DRIVER_SERVICE_AREAS_FAILURE,
-      ],
-      endPoint: `dspr/driver-service-area`,
-      schema: Schemas.DSPR_DRIVER_SERVICE_AREA_ARRAY,
-      queryParamsMap,
-    },
-  };
+    const queryParamsMap = {
+        dspr_id: dsprId,
+    };
+    return {
+        [CALL_API]: {
+            httpAction: 'GET',
+            types: [
+                GET_DSPR_DRIVER_SERVICE_AREAS,
+                GET_DSPR_DRIVER_SERVICE_AREAS_SUCCESS,
+                GET_DSPR_DRIVER_SERVICE_AREAS_FAILURE,
+            ],
+            endPoint: `dspr/driver-service-area`,
+            schema: Schemas.DSPR_DRIVER_SERVICE_AREA_ARRAY,
+            queryParamsMap,
+        },
+    };
 };
 
 export const getAllDsprDriverServiceAreasForDspr = (dsprId: string) => (dispatch) => {
-  return dispatch(getDsprDriverServiceAreas(dsprId));
+    return dispatch(getDsprDriverServiceAreas(dsprId));
 };
+
+export const GET_DSPR_ACTIVE_METRC_TAGS = 'GET_DSPR_ACTIVE_METRC_TAGS';
+export const GET_DSPR_ACTIVE_METRC_TAGS_SUCCESS = 'GET_DSPR_ACTIVE_METRC_TAGS_SUCCESS';
+export const GET_DSPR_ACTIVE_METRC_TAGS_FAILURE = 'GET_DSPR_ACTIVE_METRC_TAGS_FAILURE';
+
+const dsprActiveMetrcTagsGetter = (dsprId: number) => {
+    return {
+        [CALL_API]: {
+            httpAction: 'GET',
+            types: [
+                GET_DSPR_ACTIVE_METRC_TAGS,
+                GET_DSPR_ACTIVE_METRC_TAGS_SUCCESS,
+                GET_DSPR_ACTIVE_METRC_TAGS_FAILURE,
+            ],
+            endPoint: `metrc/association/active`,
+            schema: Schemas.DSPR_DRIVER_SERVICE_AREA_ARRAY,
+            queryParamsMap: { dsprId }
+        },
+    };
+}
+
+export const getActiveMetrcTagsForDSPR = (dsprId: number) => (dispatch) => {
+    return dispatch(dsprActiveMetrcTagsGetter(dsprId));
+}
