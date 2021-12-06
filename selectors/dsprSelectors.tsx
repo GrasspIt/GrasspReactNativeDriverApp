165b514dafc17cd6fae3cc1fe556460cf790db71
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2021-12-06 13:09:14 -0800

    Refactor selector for activeMetrcTagsForAutoComplete
    - rather than returning entire metrcTagProductAssociation, selector returns an array of objects with the keys "id" and "title". This satisfies the requirements for the dataprop on the AutoComplete component

--- selectors/dsprSelectors.tsx
+++ selectors/dsprSelectors.tsx
@@ -18,3 +18,24 @@ export const isNonMetrcScanningDSPRFromProps = createSelector(
 export const isScanningRequiredForDSPRFromProps = createSelector(
     [getDSPRFromProps], (dspr) => !!(dspr && dspr.isScanOrderDetail)
 )
+
+export const getDSPRMetrcTagsFromProps = (state: State, {dsprId}) => {
+    return state.api.entities.dsprMetrcTags[dsprId];
+}
+
+export interface ActiveMetrcTagForAutoComplete {
+    id: string;
+    title: string;
+}
+
+export const getDSPRActiveMetrcTagsForAutoComplete = createSelector(
+    [getDSPRMetrcTagsFromProps], (dsprMetrcTags) => {
+        return dsprMetrcTags
+            ? Object.values(dsprMetrcTags)
+            .filter(metrcTag => metrcTag.isActive === true)
+            .map(metrcTag => {
+                return {'id': metrcTag.id.toString(), 'title': metrcTag.metrcTag}
+                 })
+            : []
+    }
+);
