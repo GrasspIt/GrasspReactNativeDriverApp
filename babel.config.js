d07d1968d48780271b4d69ca226390cd946a722e
IntuitiveInformatics <tanielse@uci.edu>
2023-02-23 23:57:46 -0800

    Testing, IOS working, broken android

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,17 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    env: {
-      production: {
-        plugins: ['react-native-paper/babel'],
-      },
-    },
+    // env: {
+    //   prod: {
+    //     plugins: ['react-native-paper/babel', 'react-native-reanimated/plugin'],
+    //   },
+    //   dev: {
+    //     plugins: ['react-native-reanimated/plugin'],
+    //   }, 
+    //   staging: {
+    //     plugins: ['react-native-reanimated/plugin'],
+    //   }
+    // },
+    plugins: ['react-native-paper/babel', '@babel/plugin-proposal-export-namespace-from','react-native-reanimated/plugin'],
   };
 };
